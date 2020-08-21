aadd40d3e40d8907e18b7a5e4a76b985417bc46e
Duke <dukelin@va7c>
2020-08-21 11:23:33 +0800

    init

---
+++ apiTEST/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace apiTEST.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: API
+        public string call_me(string Data)
+        {
+            return Data.Length.ToString();
+        }
+    }
+}
